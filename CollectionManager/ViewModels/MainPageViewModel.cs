0a8b6ad93de64e6929d87a9a4b0e75b6450fa0a5
southernwind <testakina@gmail.com>
2019-11-11 11:35:47 +0900

    Add project files.

---
+++ CollectionManager/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Reactive.Bindings;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Media;
+
+namespace CollectionManager.ViewModels {
+	internal class MainPageViewModel {
+		public ReactiveCollection<BitmapImage> Files {
+			get;
+			set;
+		} = new ReactiveCollection<BitmapImage>();
+
+		public MainPageViewModel() {
+		}
+
+		public async Task LoadFiles() {
+			var files = await KnownFolders.PicturesLibrary.GetFilesAsync();
+			foreach (var file in files) {
+				var stream = await file.OpenReadAsync();
+				var bi = new BitmapImage();
+				await bi.SetSourceAsync(stream);
+				this.Files.Add(bi);
+			}
+		}
+	}
+}
