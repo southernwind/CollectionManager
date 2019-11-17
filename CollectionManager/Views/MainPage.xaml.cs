e789bf5d02e0c18152fd688dea70dce1dc24f053
southernwind <testakina@gmail.com>
2019-11-17 17:53:30 +0900

    リファクタリング

--- CollectionManager/Views/MainPage.xaml.cs
+++ CollectionManager/Views/MainPage.xaml.cs
@@ -1,22 +1,19 @@
-using Windows.UI.Xaml.Controls;
 using CollectionManager.ViewModels;
-using System.Threading.Tasks;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace CollectionManager {
+namespace CollectionManager.Views {
 	/// <summary>
 	/// An empty page that can be used on its own or navigated to within a Frame.
 	/// </summary>
-	public sealed partial class MainPage : Page {
+	public sealed partial class MainPage {
 		internal MainPageViewModel ViewModel {
 			get;
 		} = new MainPageViewModel();
 
-		public MainPage()
-        {
-            this.InitializeComponent();
-        }
+		public MainPage() {
+			this.InitializeComponent();
+		}
 
 		private async void Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e) {
 
