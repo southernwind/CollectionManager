7deb73ee3c52bbe7c35f5d49aacb6026ba14126f
southernwind <testakina@gmail.com>
2019-11-18 11:46:48 +0900

    設定ページの追加

--- CollectionManager/Views/MainPage.xaml.cs
+++ CollectionManager/Views/MainPage.xaml.cs
@@ -1,4 +1,7 @@
+using System;
 using CollectionManager.ViewModels;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Animation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -7,17 +10,24 @@ namespace CollectionManager.Views {
 	/// An empty page that can be used on its own or navigated to within a Frame.
 	/// </summary>
 	public sealed partial class MainPage {
-		internal MainPageViewModel ViewModel {
-			get;
-		} = new MainPageViewModel();
 
 		public MainPage() {
 			this.InitializeComponent();
 		}
 
-		private async void Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e) {
+		private void NavigationView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args) {
+			Type page;
+			if (args.IsSettingsInvoked == true) {
+				page = typeof(SettingsPage);
+			} else {
+				page = typeof(ContentPage);
+			}
 
-			await this.ViewModel.LoadFiles();
+			if (this.ContentFrame.CurrentSourcePageType == page) {
+				return;
+			}
+
+			this.ContentFrame.Navigate(page, null, args.RecommendedNavigationTransitionInfo);
 		}
 	}
 }
