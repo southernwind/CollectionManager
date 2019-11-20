98c911cfe8d2bf7203b033291265a75af47a50fe
southernwind <testakina@gmail.com>
2019-11-20 12:27:30 +0900

    Prism用コードの追加

--- CollectionManager/App.xaml.cs
+++ CollectionManager/App.xaml.cs
@@ -1,23 +1,19 @@
-using System;
-using System.IO;
-using System.Linq;
-using System.Threading;
 using System.Windows;
-using CollectionManager.ViewModels;
-using Reactive.Bindings;
-
+using Prism.Ioc;
+using CollectionManager.Views;
 
 namespace CollectionManager {
 	/// <summary>
 	/// App.xaml の相互作用ロジック
 	/// </summary>
 	public partial class App {
-		protected override void OnStartup(StartupEventArgs e) {
-			// 画面起動
-			this.MainWindow = new Views.MainWindow {
-				DataContext = new MainWindowViewModel()
-			};
-			this.MainWindow.ShowDialog();
+
+		protected override Window CreateShell() {
+			return this.Container.Resolve<MainWindow>();
+		}
+
+		protected override void RegisterTypes(IContainerRegistry containerRegistry) {
+
 		}
 	}
 }
--- CollectionManager/ViewModels/MainWindowViewModel.cs
+++ CollectionManager/ViewModels/MainWindowViewModel.cs
@@ -3,20 +3,29 @@ using System.IO;
 using System.Threading.Tasks;
 
 using CollectionManager.Composition.Base;
-
+using CollectionManager.Models;
 using Reactive.Bindings;
 
 namespace CollectionManager.ViewModels {
 	internal class MainWindowViewModel : ViewModelBase {
-		public ReactiveCollection<string> Files {
+		public ReactiveCollection<ItemSet> ItemSetList {
 			get;
-			set;
-		} = new ReactiveCollection<string>();
+		} = new ReactiveCollection<ItemSet>();
 
 		public MainWindowViewModel() {
-		}
+			var itemset = new ItemSet();
+			itemset.Authors.Value = new[] { "ADA" };
+			itemset.Title.Value = "Aqua Journal";
+			var item289 = new Item();
+			item289.FilePath.Value = "./[289]ネイチャーアクアリウム 陰性水草の印象.pdf";
+			item289.Ordinal.Value = new Ordinal() { Number = 289 };
+			itemset.ItemList.Add(item289);
+			var item288 = new Item();
+			item288.FilePath.Value = "./[288]失われた潟／古木と芽吹き.pdf";
+			item288.Ordinal.Value = new Ordinal() { Number = 289 };
+			itemset.ItemList.Add(item288);
 
-		public async Task LoadFiles() {
+			this.ItemSetList.Add(itemset);
 		}
 	}
 }
