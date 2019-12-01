908ee652895e1ee051342ca67dc7f6588dd77557
southernwind <testakina@gmail.com>
2019-12-01 13:47:16 +0900

    ファイルの表示方法をファイル名に変更

--- CollectionManager/Models/Item.cs
+++ CollectionManager/Models/Item.cs
@@ -1,9 +1,18 @@
+using System.IO;
+using System.Reactive.Linq;
+
 using CollectionManager.Composition.Base;
 
 using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
 
 namespace CollectionManager.Models {
 	internal class Item : ModelBase {
+
+		public IReadOnlyReactiveProperty<string> FileName {
+			get;
+		}
+
 		public IReactiveProperty<string> FilePath {
 			get;
 		} = new ReactivePropertySlim<string>();
@@ -15,5 +24,9 @@ namespace CollectionManager.Models {
 		public IReactiveProperty<Ordinal> Ordinal {
 			get;
 		} = new ReactivePropertySlim<Ordinal>();
+
+		public Item() {
+			this.FileName = this.FilePath.Select(Path.GetFileName).ToReadOnlyReactiveProperty().AddTo(this.CompositeDisposable);
+		}
 	}
 }
